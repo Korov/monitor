cf531ae5efb44709026173c865571c9300ed949d
Korov <korov9@163.com>
2022-01-23 13:09:37 +0800

    reactive message

---
+++ src/main/java/org/korov/monitor/websocket/CostCollector.java
@@ -0,0 +1,31 @@
+package org.korov.monitor.websocket;
+
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/collected-costs")
+@ApplicationScoped
+public class CostCollector {
+    private static final Logger logger = LoggerFactory.getLogger(CostCollector.class);
+    private double sum;
+
+    @GET
+    // expose the sum of the collected costs
+    public synchronized double getCosts() {
+        return sum;
+    }
+
+    @Incoming("collector")
+    // consume costs from collector channel
+    synchronized void collect(Cost cost) {
+        if ("EUR".equals(cost.getCurrency())) {
+            sum += cost.getValue();
+        }
+        logger.info("sum:{}", sum);
+    }
+}
