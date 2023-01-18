76c35a493ead5b50cad877314b1d24162ca5bb29
Korov <korov9@163.com>
2023-01-18 11:42:01 +0800

    spring web flux enable start up

--- backend/spring_webflux/src/main/java/org/korov/monitor/MonitorApplication.java
+++ backend/spring_webflux/src/main/java/org/korov/monitor/MonitorApplication.java
@@ -2,11 +2,13 @@ package org.korov.monitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 /**
  * @author korov
  */
+//@EnableAspectJAutoProxy
 @SpringBootApplication
 public class MonitorApplication {
     public static void main(String[] args) {
