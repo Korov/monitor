ff89069970204226e5ef164903bbc234b29289a5
Korov <korov9@163.com>
2023-01-18 14:39:57 +0800

    change proxy address

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
     port: 18091,
     proxy: {
       '^/api': {
-        target: 'http://127.0.0.1:8091',
+        target: 'http://localhost:8091',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
