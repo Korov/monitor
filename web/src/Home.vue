18460abf543568bf5adfffb2400c8ec6e050af2a
Korov <korov9@163.com>
2023-04-16 21:36:30 +0800

    update index vue

--- web/src/Home.vue
+++ web/src/Home.vue
@@ -8,8 +8,15 @@
     <el-row>
       <el-col :span="leftSpan" class="navLeft">
         <div>
-          <svg :class="{ 'is-active': isCollapse }" class="hamburger" viewBox="0 0 1024 1024" @click="collapseHandle()"
-               xmlns="http://www.w3.org/2000/svg" width="64" height="64">
+          <svg
+            :class="{ 'is-active': isCollapse }"
+            class="hamburger"
+            viewBox="0 0 1024 1024"
+            @click="collapseHandle()"
+            xmlns="http://www.w3.org/2000/svg"
+            width="64"
+            height="64"
+          >
             <path
               d="M408 442h480c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8H408c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8zm-8 204c0 4.4 3.6 8 8 8h480c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8H408c-4.4 0-8 3.6-8 8v56zm504-486H120c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0 632H120c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zM142.4 642.1L298.7 519a8.84 8.84 0 0 0 0-13.9L142.4 381.9c-5.8-4.6-14.4-.5-14.4 6.9v246.3a8.9 8.9 0 0 0 14.4 7z"
             />
@@ -53,49 +60,49 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref } from "vue";
-import { useRoute } from "vue-router";
-import { localStorage } from "@/stores/store";
+import { computed, defineComponent, ref } from 'vue'
+import { useRoute } from 'vue-router'
+import { localStorage } from '@/stores/store'
 
 export default defineComponent({
-  name: "HomePage",
+  name: 'HomePage',
   setup() {
-    const isCollapse = ref(true);
-    const leftSpan = ref(2);
-    const rightSpan = ref(22);
-    const route = useRoute();
+    const isCollapse = ref(true)
+    const leftSpan = ref(2)
+    const rightSpan = ref(22)
+    const route = useRoute()
     let activePath = computed(() => {
-      if (route.path === "/") {
-        return "/kafka/config";
+      if (route.path === '/') {
+        return '/kafka/config'
       } else {
-        return route.path;
+        return route.path
       }
-    });
+    })
 
     function initCollapse() {
-      isCollapse.value = Boolean(localStorage.get("isCollapse") === "true");
+      isCollapse.value = Boolean(localStorage.get('isCollapse') === 'true')
       if (isCollapse.value) {
-        leftSpan.value = 2;
-        rightSpan.value = 24 - leftSpan.value;
+        leftSpan.value = 2
+        rightSpan.value = 24 - leftSpan.value
       } else {
-        leftSpan.value = 4;
-        rightSpan.value = 24 - leftSpan.value;
+        leftSpan.value = 4
+        rightSpan.value = 24 - leftSpan.value
       }
     }
 
-    initCollapse();
+    initCollapse()
 
     function collapseHandle() {
       if (isCollapse.value) {
-        isCollapse.value = !isCollapse.value;
-        leftSpan.value = 4;
-        rightSpan.value = 24 - leftSpan.value;
+        isCollapse.value = !isCollapse.value
+        leftSpan.value = 4
+        rightSpan.value = 24 - leftSpan.value
       } else {
-        isCollapse.value = !isCollapse.value;
-        leftSpan.value = 2;
-        rightSpan.value = 24 - leftSpan.value;
+        isCollapse.value = !isCollapse.value
+        leftSpan.value = 2
+        rightSpan.value = 24 - leftSpan.value
       }
-      localStorage.set("isCollapse", String(isCollapse.value));
+      localStorage.set('isCollapse', String(isCollapse.value))
     }
 
     return {
@@ -103,10 +110,10 @@ export default defineComponent({
       isCollapse,
       leftSpan,
       rightSpan,
-      collapseHandle
-    };
-  }
-});
+      collapseHandle,
+    }
+  },
+})
 </script>
 
 <style lang="scss" scoped>
