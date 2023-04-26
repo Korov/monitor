50729c9ec07639805ee55d768e749f8ecc462379
Korov <korov9@163.com>
2023-04-26 09:44:58 +0800

    fix:修复kafka docker相关问题

--- backend/spring_webflux/src/main/java/org/korov/monitor/utils/ZookeeperUtils.java
+++ backend/spring_webflux/src/main/java/org/korov/monitor/utils/ZookeeperUtils.java
@@ -1,15 +1,30 @@
 package org.korov.monitor.utils;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.ClientInfo;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author zhu.lei
  * @date 2023-04-16 22:55
  */
+@Slf4j
 public class ZookeeperUtils {
+
     public static ZooKeeper getZookeeper(String host) throws IOException {
         return new ZooKeeper(host, 20000, null);
+    }
+
+    public static List<ClientInfo> getAllClientInfos(String host) throws IOException {
+        try (ZooKeeper zooKeeper = ZookeeperUtils.getZookeeper(host)) {
+            return zooKeeper.whoAmI();
+        } catch (Exception e) {
+            log.error("error:{}", host, e);
+            return Collections.emptyList();
+        }
     }
 }
--- backend/spring_webflux/src/test/java/org/korov/monitor/utils/ZookeeperUtilsTest.java
+++ backend/spring_webflux/src/test/java/org/korov/monitor/utils/ZookeeperUtilsTest.java
@@ -1,11 +1,15 @@
 package org.korov.monitor.utils;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooKeeper;
-import org.apache.zookeeper.data.ClientInfo;
+import org.apache.zookeeper.data.ACL;
+import org.apache.zookeeper.data.Stat;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,12 +21,25 @@ class ZookeeperUtilsTest {
 
     @Test
     public void getZookeeper() throws IOException, InterruptedException {
+
         try (ZooKeeper zooKeeper = ZookeeperUtils.getZookeeper("localhost:2183")) {
-            List<ClientInfo> clientInfos = zooKeeper.whoAmI();
-            for (ClientInfo clientInfo : clientInfos) {
-                log.info(clientInfo.toString());
-            }
+            ZooKeeper.States states = zooKeeper.getState();
+            log.info("zookeeper states:{}", states);
+
+            int count = zooKeeper.getAllChildrenNumber("/");
+            Stat stat = new Stat();
+            List<String> childPaths = zooKeeper.getChildren("/zookeeper", false, stat);
+            Stat dataStat = new Stat();
+            byte[] data = zooKeeper.getData("/zookeeper/config", false, dataStat);
+            log.info("debug");
+
+            List<ACL> acl = new ArrayList<>();
+            acl.add(new ACL());
+            zooKeeper.create("/data_test", "message".getBytes(StandardCharsets.UTF_8), )
+        } catch (KeeperException e) {
+            throw new RuntimeException(e);
         }
+        log.info("debug");
     }
 
 }
