9f58c4ea5d66b813d857028864f4f905d370d7b8
Korov <korov9@163.com>
2023-06-07 16:59:28 +0800

    update build readme

--- backend/spring_webflux/README.md
+++ backend/spring_webflux/README.md
@@ -1,9 +1,14 @@
 # Build
 
 ```bash
+# build docker image
 ./mvnw spring-boot:build-image -Pnative
 
-./mvnw -Pnative spring-boot:build-image -DskipTests
+./mvnw spring-boot:build-image -Pnative -DskipTests
 
+# build executable binary
+# in windows you need add profile `"C:\Program Files\PowerShell\7\pwsh.exe" -NoExit -Command "&{Import-Module """C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"""; Enter-VsDevShell 078f62ad -SkipAutomaticLocation -DevCmdArguments """-arch=x64 -host_arch=x64"""}"` to windows terminal to build
 ./mvnw native:compile -Pnative
+
+./mvnw native:compile -Pnative -DskipTests
 ```
--- backend/spring_webflux/src/test/java/org/korov/monitor/MonitorApplicationTests.java
+++ backend/spring_webflux/src/test/java/org/korov/monitor/MonitorApplicationTests.java
@@ -2,11 +2,10 @@ package org.korov.monitor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.korov.monitor.vo.Result;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.reactive.server.FluxExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @Slf4j
@@ -14,14 +13,20 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 @AutoConfigureWebTestClient
 public class MonitorApplicationTests {
     protected WebTestClient webClient;
+    private String profile;
 
     @Autowired
     public void setWebClient(WebTestClient webClient) {
         this.webClient = webClient;
     }
 
+    @Autowired
+    public void setProfile(@Value("${profile}") String profile) {
+        this.profile = profile;
+    }
+
     @Test
     void contextLoads() {
-        log.info("success");
+        log.info("start with profile:{}", profile);
     }
 }
