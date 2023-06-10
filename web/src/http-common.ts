6857d5b507c311c39afb0003029e6485ef122900
Korov <korov9@163.com>
2023-06-10 19:19:15 +0800

    modify style

--- web/src/http-common.ts
+++ web/src/http-common.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios'
 
 const apiClient: AxiosInstance = axios.create({
-  baseURL: 'http://172.21.227.18:18091/api',
+  baseURL: 'http://localhost:18091/api',
   headers: {
     Accept: 'application/json',
     'Content-type': 'application/json',
