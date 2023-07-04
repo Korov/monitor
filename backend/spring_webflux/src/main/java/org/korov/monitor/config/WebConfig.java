9a04765eecd50f412b952ab8e62fd00fffc29f68
Korov <korov9@163.com>
2023-07-04 14:56:17 +0800

    support cros

---
+++ backend/spring_webflux/src/main/java/org/korov/monitor/config/WebConfig.java
@@ -0,0 +1,21 @@
+package org.korov.monitor.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+/**
+ * @author zhu.lei
+ * @date 2023-07-04 14:53
+ */
+@Configuration
+public class WebConfig implements WebFluxConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
+}
