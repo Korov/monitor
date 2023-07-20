f98f432793514e9cd76d46f0518f1f18181b1d34
Korov <korov9@163.com>
2023-07-20 22:12:31 +0800

    add test for kafka test

--- backend/axum/tests/kafka_test.rs
+++ backend/axum/tests/kafka_test.rs
@@ -1,13 +1,17 @@
 #[cfg(test)]
 mod tests {
+    use kafka::client::KafkaClient;
     use log::info;
 
     #[test]
     fn print() {
+        log4rs::init_file("log4rs.yml", Default::default()).unwrap();
         info!("test");
-        let brokers = vec!["localhost:9092"];
-        let client_config = kafka::client::KafkaClientConfig::new(&brokers);
+        let mut brokers = Vec::new();
+        brokers.push("localhost:9092".to_string());
         // Create a Kafka client
-        let kafka_client = KafkaClient::new(client_config).unwrap();
+        let mut kafka_client = KafkaClient::new(brokers);
+        let all_metadata = kafka_client.load_metadata_all();
+        info!("metadata:{:?}", all_metadata.unwrap());
     }
 }
