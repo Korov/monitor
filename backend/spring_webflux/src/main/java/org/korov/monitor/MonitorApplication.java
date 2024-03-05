74f7a9b30dd4cd4932fdd26865dc5ed1ae59a950
Korov <korov9@163.com>
2024-03-05 16:23:02 +0800

    remove unused import

--- backend/spring_webflux/src/main/java/org/korov/monitor/MonitorApplication.java
+++ backend/spring_webflux/src/main/java/org/korov/monitor/MonitorApplication.java
@@ -2,8 +2,6 @@ package org.korov.monitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.web.reactive.config.EnableWebFlux;
 
 /**
  * @author korov
