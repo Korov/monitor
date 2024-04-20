d4857bca1ef4eeb36733b05c1e8c6f67053147ca
Korov <korov9@163.com>
2024-04-20 15:16:22 +0800

    upgrade: upgrade rust

--- backend/axum/src/main.rs
+++ backend/axum/src/main.rs
@@ -1,13 +1,11 @@
 use monitor_lib::create_app;
-use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
     let app = create_app().await;
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app)
         .await
         .unwrap();
 }
